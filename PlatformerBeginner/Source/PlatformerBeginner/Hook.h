1c5109a30ffe272fddade970f73b984c263b35ba
prasopes <25298569+insighthorizon@users.noreply.github.com>
2022-01-16 23:03:19 +0100

    edit

---
+++ PlatformerBeginner/Source/PlatformerBeginner/Hook.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
+#include "Hook.generated.h"
+
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PLATFORMERBEGINNER_API UHook : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHook();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	float Reach = 100.f;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	UInputComponent* InputComponent = nullptr;
+
+	void GetComponentAndBind();
+
+	void Grab();
+	void Release();
+
+	FHitResult GetPhysicsBodyInReach();
+
+	FVector GetReachStart();
+	FVector GetReachEnd();
+		
+};
---
+++ PlatformerBeginner/Source/PlatformerBeginner/PlatformerBeginner.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlatformerBeginner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlatformerBeginner, "PlatformerBeginner" );
